f5b579f16e9315c774b159ca02457583319835b9
joel-maws <joel@mobileaws.com>
2022-02-08 15:48:19 -0800

    hard hat changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
+
+require('dotenv').config();
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const INFURA_ID = process.env.INFURA_ID;
+
 // const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
@@ -9,20 +15,13 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    /*
     mumbai: {
-      // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [privateKey]
-    },
-    matic: {
-      // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [privateKey]
+      url: "https://polygon-mumbai.infura.io/v3/${INFURA_ID}",
+      accounts: [PRIVATE_KEY]
     }
-    */
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   },
   solidity: {
     version: "0.8.4",
@@ -34,4 +33,5 @@ module.exports = {
     }
   }
 };
+
 
